6ded81d71e23628c1fa8b9829d5772d5cf3791d4
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-23 21:46:45 -0200

    Installing ejs and creating the technology view

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 
 
-app.get('/', function(req, res) {
-    res.send('ola');
+app.set('view engine', 'ejs');
+
+app.get('/tecnologia', function(req, res) {
+    res.render('secao/tecnologia');
 });
 
 app.listen(3000, function() {
