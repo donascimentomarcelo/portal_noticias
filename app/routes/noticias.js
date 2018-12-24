c01cd57d176d5ccde800fd69d3eab52f97e0d4bb
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-24 09:30:06 -0200

    Configuring mysql and creating the first query.

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,18 @@
 module.exports = function(app) {
+
     app.get('/noticias', function(req, res) {
-        res.render('noticias/noticias');
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : '',
+            database : 'portal_noticias',
+        });
+    
+        var query = 'SELECT * FROM noticias';
+        connection.query(query, function(error, result){
+            res.send(result);
+        });
     });
 }
