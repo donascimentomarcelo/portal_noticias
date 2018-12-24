210fb5f4eab4df45edb1b89502b7a9cd52e43e2b
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-12-24 09:57:45 -0200

    Refactoring route removing database connection and creating a specific module for export database.

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,15 +1,7 @@
+var connection = require('../../config/db-connection')();
+
 module.exports = function(app) {
-
     app.get('/noticias', function(req, res) {
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : '',
-            database : 'portal_noticias',
-        });
-    
         var query = 'SELECT * FROM noticias';
         connection.query(query, function(error, result){
             res.render('noticias/noticias', {noticias : result});
