d546adaa285962c3b3c503672b24617809498bde
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-01-02 22:22:46 -0200

    Refactoring method to get the database connection.

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,15 +1,17 @@
 module.exports.save = function(application, req, res) {
-    var dataForm = req.body;
-
-    var connection = application.config.db_connection;
-    var UserDAO = new application.app.models.UsersDAO(connection);
+    const dataForm = req.body;
+    const UserDAO = getConnection(application);
 
     UserDAO.save(dataForm, res);
 }
 
-module.exports.find = function(application, req, res) {
-    var connection = application.config.db_connection;
-    var UserDAO = new application.app.models.UsersDAO(connection);
+module.exports.getAll = function(application, req, res) {
+    const UserDAO = getConnection(application);
+    UserDAO.getAll(res);
+}
 
-    UserDAO.find(res);
+function getConnection(application) {
+    const connection = application.config.db_connection;
+    const UserDAO = new application.app.models.UsersDAO(connection);
+    return UserDAO
 }
